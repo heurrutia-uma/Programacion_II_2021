943f6b5e96e52174d7644f4c26ba5583910a65f2
E059994 <E059994@mcgladrey.rsm.net>
2021-02-20 12:26:02 -0600

    Unidad2a

---
+++ 2021/Unidad2a/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Unidad2a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnMostrar = new System.Windows.Forms.Button();
+            this.LstClientes = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LblNombre = new System.Windows.Forms.Label();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.LblApellido = new System.Windows.Forms.Label();
+            this.TxtApellido = new System.Windows.Forms.TextBox();
+            this.LblEmail = new System.Windows.Forms.Label();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnMostrar
+            // 
+            this.BtnMostrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnMostrar.Location = new System.Drawing.Point(559, 219);
+            this.BtnMostrar.Name = "BtnMostrar";
+            this.BtnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.BtnMostrar.TabIndex = 1;
+            this.BtnMostrar.Text = "Mostrar";
+            this.BtnMostrar.UseVisualStyleBackColor = true;
+            this.BtnMostrar.Click += new System.EventHandler(this.BtnMostrar_Click);
+            // 
+            // LstClientes
+            // 
+            this.LstClientes.FormattingEnabled = true;
+            this.LstClientes.Location = new System.Drawing.Point(6, 19);
+            this.LstClientes.Name = "LstClientes";
+            this.LstClientes.Size = new System.Drawing.Size(336, 160);
+            this.LstClientes.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LstClientes);
+            this.groupBox2.Location = new System.Drawing.Point(293, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(348, 195);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lista de Clientes";
+            // 
+            // LblNombre
+            // 
+            this.LblNombre.AutoSize = true;
+            this.LblNombre.Location = new System.Drawing.Point(6, 16);
+            this.LblNombre.Name = "LblNombre";
+            this.LblNombre.Size = new System.Drawing.Size(44, 13);
+            this.LblNombre.TabIndex = 0;
+            this.LblNombre.Text = "Nombre";
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(9, 32);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(241, 20);
+            this.TxtNombre.TabIndex = 1;
+            // 
+            // LblApellido
+            // 
+            this.LblApellido.AutoSize = true;
+            this.LblApellido.Location = new System.Drawing.Point(6, 55);
+            this.LblApellido.Name = "LblApellido";
+            this.LblApellido.Size = new System.Drawing.Size(44, 13);
+            this.LblApellido.TabIndex = 2;
+            this.LblApellido.Text = "Apellido";
+            // 
+            // TxtApellido
+            // 
+            this.TxtApellido.Location = new System.Drawing.Point(9, 71);
+            this.TxtApellido.Name = "TxtApellido";
+            this.TxtApellido.Size = new System.Drawing.Size(241, 20);
+            this.TxtApellido.TabIndex = 3;
+            // 
+            // LblEmail
+            // 
+            this.LblEmail.AutoSize = true;
+            this.LblEmail.Location = new System.Drawing.Point(6, 94);
+            this.LblEmail.Name = "LblEmail";
+            this.LblEmail.Size = new System.Drawing.Size(32, 13);
+            this.LblEmail.TabIndex = 4;
+            this.LblEmail.Text = "Email";
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.Location = new System.Drawing.Point(9, 110);
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(241, 20);
+            this.TxtEmail.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtEmail);
+            this.groupBox1.Controls.Add(this.LblEmail);
+            this.groupBox1.Controls.Add(this.TxtApellido);
+            this.groupBox1.Controls.Add(this.LblApellido);
+            this.groupBox1.Controls.Add(this.TxtNombre);
+            this.groupBox1.Controls.Add(this.LblNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 195);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Personales";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 260);
+            this.Controls.Add(this.BtnMostrar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(250, 240);
+            this.Name = "Form1";
+            this.Text = "Información de Clientes";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnMostrar;
+        private System.Windows.Forms.ListBox LstClientes;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label LblNombre;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label LblApellido;
+        private System.Windows.Forms.TextBox TxtApellido;
+        private System.Windows.Forms.Label LblEmail;
+        private System.Windows.Forms.TextBox TxtEmail;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ 2021/Unidad2a/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Unidad2a
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            var rutaArchivo = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Archivo CSV (*.csv)|*.csv|Archivo de Excel (*.xlsx)|*.xlsx";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                { 
+                    rutaArchivo = openFileDialog.FileName;
+
+                    using (StreamReader reader = new StreamReader(rutaArchivo))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            LstClientes.Items.Add(reader.ReadLine());   
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
